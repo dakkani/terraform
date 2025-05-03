a88866a01a08ca987f1aeae3dd2db0800ce40890
dakkani <135724674+dakkani@users.noreply.github.com>
2025-05-03 12:03:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (us-east-1)
+  ami           = "ami-005f0d81384686e83" # SLES AMI (us-east-1)
   instance_type = "t2.micro"
 
   tags = {
